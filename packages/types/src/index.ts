401b4534d5b9855dc73287ae355fea754c1cc4d8
r0bi7 <robi.markac@gmail.com>
2026-08-06 17:35:44 +0200

    chore: bump version

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -20,4 +20,4 @@ export * from './swap/index.js';
 export * from './utils/index.js';
 export * from './wallet/index.js';
 
-export const CONFIG_VERSION = 222; // this value should be incremented (inside release/sdk branch) each time @sodax/types package is updated
+export const CONFIG_VERSION = 223; // this value should be incremented (inside release/sdk branch) each time @sodax/types package is updated
